50df4fc1f8191db71478ff9af2f7152285830b6d
imsonmia <imsonmia@outlook.com>
2023-08-02 09:12:18 +0800

    append put/delete methods to transactcontroller

--- src/main/java/me/imsonmia/demoapi/TransactionsRepository/TransactionController.java
+++ src/main/java/me/imsonmia/demoapi/TransactionsRepository/TransactionController.java
@@ -4,9 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -59,4 +61,24 @@ public class TransactionController {
         return repository.save(transaction);
     }
 
+    @PutMapping("/put/{id}")
+    Transaction replaceTransaction(@RequestBody Transaction newTransaction, @PathVariable Long id) {
+        return repository.findById(id)
+                .map(transaction -> {
+                    transaction.setTransactionAmountCents(newTransaction.getTransactionAmountCents());
+                    transaction.setSenderId(newTransaction.getSenderId());
+                    transaction.setReceiverId(newTransaction.getReceiverId());
+                    transaction.setNote(newTransaction.getNote());
+
+                    return repository.save(transaction);
+                }).orElseGet(() -> {
+                    newTransaction.setId(id);
+                    return repository.save(newTransaction);
+                });
+    }
+
+    @DeleteMapping("/delete/{id}")
+    void deleteTransaction(@PathVariable Long id) {
+        repository.deleteById(id);
+    }
 }
