d6e259690822efc215b1861ff984f3b3b0b6c2e3
Zhongheng Liu <imsonmia@outlook.com>
2023-11-09 22:48:54 +0200

    faker random data generation test

--- src/main/java/me/imsonmia/demoapi/employee/LoadEmployeeDatabase.java
+++ src/main/java/me/imsonmia/demoapi/employee/LoadEmployeeDatabase.java
@@ -6,9 +6,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-// import me.imsonmia.demoapi.employee.Employee.Level;
-// import me.imsonmia.demoapi.employee.Employee.Role;
-// import net.datafaker.Faker;
+import me.imsonmia.demoapi.employee.Employee.Level;
+import me.imsonmia.demoapi.employee.Employee.Role;
+import net.datafaker.Faker;
 
 @Configuration
 public class LoadEmployeeDatabase {
@@ -18,20 +18,20 @@ public class LoadEmployeeDatabase {
     CommandLineRunner initDatabase(EmployeeRepository repository) {
         return args -> {
             log.info("Preloading employee repository...");
-            // Faker faker = new Faker();
-            // for (int i = 0; i < 100; i++) {
-            // final String[] employeeLevelsForFaker = { "BOARD", "UPPER", "MIDDLE",
-            // "EMPLOYEE", "TEMP" };
-            // final String[] employeeRolesForFaker = { "MAINTENANCE", "DEVELOPMENT",
-            // "LOGISTICS", "MANAGEMENT" };
-            // repository.save(new Employee(
-            // faker.name().fullName(),
-            // faker.random().nextInt(0, 1000000),
-            // faker.random().nextInt(0, 100),
-            // faker.date().birthday(),
-            // Level.valueOf(employeeLevelsForFaker[faker.random().nextInt(employeeLevelsForFaker.length)]),
-            // Role.valueOf(employeeRolesForFaker[faker.random().nextInt(employeeRolesForFaker.length)])));
-            // }
+            Faker faker = new Faker();
+            for (int i = 0; i < 100; i++) {
+            final String[] employeeLevelsForFaker = { "BOARD", "UPPER", "MIDDLE",
+            "EMPLOYEE", "TEMP" };
+            final String[] employeeRolesForFaker = { "MAINTENANCE", "DEVELOPMENT",
+            "LOGISTICS", "MANAGEMENT" };
+            repository.save(new Employee(
+            faker.name().fullName(),
+            faker.random().nextInt(0, 1000000),
+            faker.random().nextInt(0, 100),
+            faker.date().birthday(),
+            Level.valueOf(employeeLevelsForFaker[faker.random().nextInt(employeeLevelsForFaker.length)]),
+            Role.valueOf(employeeRolesForFaker[faker.random().nextInt(employeeRolesForFaker.length)])));
+            }
             for (Employee employee : repository.findAll()) {
                 log.info("Found employee: " + employee.toString());
             }
